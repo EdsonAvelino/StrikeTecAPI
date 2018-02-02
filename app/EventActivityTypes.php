0000cf4ff490a72fa99be8ba585289a3eac2a8a6
Nawaz <muhammadnmx@gmail.com>
2018-02-02 11:46:40 +0530

    #202 image url updated

--- app/EventActivityTypes.php
+++ app/EventActivityTypes.php
@@ -6,4 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 class EventActivityTypes extends Model
 {
     protected $fillable = ['name', 'image_url', 'description'];
+
+    public function getImageUrlAttribute($image)
+    {
+    	if ($image) {
+            return env('APP_URL') . '/storage/events/activities/' . $image;
+        }
+    }
 }
