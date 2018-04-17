a93c7afec36a404810bd157829a129b06f6086b5
Nawaz <muhammadnmx@gmail.com>
2018-04-17 20:35:01 +0530

    Videos update and apidoc updates

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -345,26 +345,38 @@ class VideoController extends Controller
      *      "error": "false",
      *      "message": "",
      *      "videos": [
-     *          {
-     *              "id": 1,
-     *              "title": null,
-     *              "file": "http://example.com/videos/SampleVideo_1280x720_10mb.mp4",
-     *              "thumbnail": "http://example.com/videos/thumb/SampleVideo_1280x720_10mb.png",
-     *              "views": 250,
-     *              "author_name": "Limer Waughts",
-     *              "duration": "00:01:02",
-     *              "user_favourited": true
-     *          },
-     *          {
-     *              "id": 2,
-     *              "title": null,
-     *              "file": "http://example.com/videos/SampleVideo_1280x720_20mb.mp4",
-     *              "thumbnail": "http://example.com/videos/thumb/SampleVideo_1280x720_20mb.png",
-     *              "views": 170,
-     *              "author_name": "Aeron Emeatt",
-     *              "duration": "00:01:12",
-     *              "user_favourited": true
-     *          },
+     *           {
+     *             "type_id": 3,
+     *             "plan_id": 1,
+     *             "title": "Jab-Jab-Cross",
+     *             "video_title": "Susan Kocab's Jab-Jab-Cross",
+     *             "thumbnail": "http://example.com/videos/thumbnails/thumb_video_1523734899.jpg",
+     *             "duration": "00:49",
+     *             "trainer": {
+     *                 "id": 1,
+     *                 "type": 1,
+     *                 "first_name": "Susan",
+     *                 "last_name": "Kocab"
+     *             },
+     *             "rating": "0.0",
+     *             "filter": 1
+     *         },
+     *         {
+     *             "type_id": 3,
+     *             "plan_id": 4,
+     *             "title": "Jab-Cross-Roll Right",
+     *             "video_title": "Susan Kocab's Jab-Cross-Roll Right",
+     *             "thumbnail": "http://example.com/videos/thumbnails/thumb_video_1523734966.jpg",
+     *             "duration": "00:34",
+     *             "trainer": {
+     *                 "id": 1,
+     *                 "type": 1,
+     *                 "first_name": "Susan",
+     *                 "last_name": "Kocab"
+     *             },
+     *             "rating": "0.0",
+     *             "filter": 1
+     *         }
      *      ]
      *    }
      * @apiErrorExample {json} Error Response
@@ -384,81 +396,37 @@ class VideoController extends Controller
 
         $videos = [];
 
-        foreach ($_videos as $video) {
-            $userFavourited = UserFavVideos::where('user_id', \Auth::id())->where('video_id', $video->id)->exists();
+        // foreach ($_videos as $video) {
+        //     $userFavourited = UserFavVideos::where('user_id', \Auth::id())->where('video_id', $video->id)->exists();
 
-            $video['user_favourited'] = $userFavourited;
+        //     $video['user_favourited'] = $userFavourited;
 
-            $planDetails = null;
+        //     $planDetails = null;
 
-            switch ($video->type_id) {
-                case \App\Types::COMBO:
-                    $planDetails = \App\Combos::get($video->plan_id);
-                    break;
-                case \App\Types::COMBO_SET:
-                    $planDetails = \App\ComboSets::get($video->plan_id);
-                    break;
-                case \App\Types::WORKOUT:
-                    $planDetails = \App\Workouts::getOptimized($video->plan_id);
-                    break;
-            }
+        //     switch ($video->type_id) {
+        //         case \App\Types::COMBO:
+        //             $planDetails = \App\Combos::get($video->plan_id);
+        //             break;
+        //         case \App\Types::COMBO_SET:
+        //             $planDetails = \App\ComboSets::get($video->plan_id);
+        //             break;
+        //         case \App\Types::WORKOUT:
+        //             $planDetails = \App\Workouts::getOptimized($video->plan_id);
+        //             break;
+        //     }
 
-            if ($planDetails) {
-                $video['plan_details'] = $planDetails;
-            }
+        //     if ($planDetails) {
+        //         $video['plan_details'] = $planDetails;
+        //     }
             
-            $videos[] = $video;
+        //     $videos[] = $video;
+        // }
+
+        foreach ($_videos as $video) {
+            $videos[] = $this->getPlanData($video);
         }
 
         return response()->json(['error' => 'false', 'message' => '', 'videos' => $videos]);
-    }
-
-    /**
-     * Getting plan data for /guidance/home (optimized object)
-     */
-    private function getPlanData($video)
-    {
-        switch ($video->type_id) {
-            // Combo
-            case \App\Types::COMBO:
-                $plan = \App\Combos::select('name', 'trainer_id', \DB::raw('id as rating'), \DB::raw('id as filter'))->where('id', $video->plan_id)->first();
-                break;
-            
-            // Combo Set
-            case \App\Types::COMBO_SET:
-                $plan = \App\ComboSets::select('name', 'trainer_id', \DB::raw('id as rating'), \DB::raw('id as filter'))->withCount('combos')->where('id', $video->plan_id)->first();
-                break;
-
-            // Workout
-            case \App\Types::WORKOUT:
-                $plan = \App\Workouts::select('name', 'trainer_id', \DB::raw('id as rating'), \DB::raw('id as filter'))->withCount('rounds')->where('id', $video->plan_id)->first();
-                break;
-
-            default:
-                // Essential Video
-                $plan = $video;
-                break;
-        }
-        
-        $data = [
-            'type_id' => $video->type_id,
-            'plan_id' => $video->plan_id,
-            'title' => $plan->name,
-            'video_title' => $video->title,
-            'thumbnail' => $video->thumbnail,
-            'duration' => $video->duration,
-            'trainer' => ['id' => $plan->trainer->id, 'type' => $plan->trainer->type, 'first_name' => $plan->trainer->first_name, 'last_name' => $plan->trainer->last_name],
-            'rating' => $plan->rating,
-            'filter' => $plan->filter
-        ];
-
-        if ($video->type_id == \App\Types::COMBO_SET) {
-            $data['combos_count'] = $plan->combos_count;
-        } elseif ($video->type_id == \App\Types::WORKOUT) {
-            $data['rounds_count'] = $plan->rounds_count;
-        }
-
-        return $data;
     }
 
     /**
@@ -639,5 +607,53 @@ class VideoController extends Controller
         $tags = $_tags->get();
 
         return response()->json(['error' => 'false', 'message' => '', 'data' => $tags]);
+    }
+
+    /**
+     * Getting plan data for /user/fav_videos
+     */
+    private function getPlanData($video)
+    {
+        switch ($video->type_id) {
+            // Combo
+            case \App\Types::COMBO:
+                $plan = \App\Combos::select('name', 'trainer_id', \DB::raw('id as rating'), \DB::raw('id as filter'))->where('id', $video->plan_id)->first();
+                break;
+            
+            // Combo Set
+            case \App\Types::COMBO_SET:
+                $plan = \App\ComboSets::select('name', 'trainer_id', \DB::raw('id as rating'), \DB::raw('id as filter'))->withCount('combos')->where('id', $video->plan_id)->first();
+                break;
+
+            // Workout
+            case \App\Types::WORKOUT:
+                $plan = \App\Workouts::select('name', 'trainer_id', \DB::raw('id as rating'), \DB::raw('id as filter'))->withCount('rounds')->where('id', $video->plan_id)->first();
+                break;
+
+            default:
+                // Essential Video
+                $plan = $video;
+                break;
+        }
+        
+        $data = [
+            'type_id' => $video->type_id,
+            'plan_id' => $video->plan_id,
+            'title' => $plan->name,
+            'video_title' => $video->title,
+            'thumbnail' => $video->thumbnail,
+            'duration' => $video->duration,
+            'trainer' => ['id' => $plan->trainer->id, 'type' => $plan->trainer->type, 'first_name' => $plan->trainer->first_name, 'last_name' => $plan->trainer->last_name],
+            'rating' => $plan->rating,
+            'filter' => $plan->filter
+        ];
+
+        if ($video->type_id == \App\Types::COMBO_SET) {
+            $data['combos_count'] = $plan->combos_count;
+        } elseif ($video->type_id == \App\Types::WORKOUT) {
+            $data['rounds_count'] = $plan->rounds_count;
+        }
+
+        return $data;
     }
 }
